4afb5bbc65edbe04c728389aabf26f54024321eb
exmarxdespot <ofgodsandmythos@gmail.com>
2018-06-08 15:48:28 -0500

    DONE: Finish the naive bayes classifier
    NEXT: Refactor it cause I need to

--- 1-classification-and-regression-using-supervised-learning/naive-bayes-classifier.py
+++ 1-classification-and-regression-using-supervised-learning/naive-bayes-classifier.py
@@ -34,3 +34,35 @@ print("Accuracy of Naive Bayes classifier =", round(accuracy, 2), "%")
 
 # Visualize the performance of the classifier
 visualize_classifier(classifier, X, y)
+
+
+# Split data into training and test data
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2, random_state=3)
+classifier_new = GaussianNB()
+classifier_new.fit(X_train, y_train)
+y_test_pred = classifier_new.predict(X_test)
+
+
+# Compute accuracy of the classifier
+accuracy = 100.0 * (y_test == y_test_pred).sum() / X_test.shape[0]
+print("Accuracy of the new classifier =", round(accuracy, 2), "%")
+
+
+# Visualize the performance of the classifier
+visualize_classifier(classifier_new, X_test, y_test)
+
+
+# Check accuracy, precision, and recall
+num_folds = 3
+
+accuracy_values = cross_validation.cross_val_score(classifier, X, y, scoring='accuracy', cv=num_folds)
+print("Accuracy: " + str(round(100*accuracy_values.mean(), 2)) + "%")
+
+precision_values = cross_validation.cross_val_score(classifier, X, y, scoring='precision_weighted', cv=num_folds)
+print("Precision: " + str(round(100*precision_values.mean(), 2)) + "%")
+
+recall_values = cross_validation.cross_val_score(classifier, X, y, scoring='recall_weighted', cv=num_folds)
+print("Recall: " + str(round(100*recall_values.mean(), 2)) + "%")
+
+f1_values = cross_validation.cross_val_score(classifier, X, y, scoring='f1_weighted', cv=num_folds)
+print("F1: " + str(round(100*f1_values.mean(), 2)) + "%")
